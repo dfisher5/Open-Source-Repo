2e1d392d9f2925fe7e1245f93d5da6b6dfbdff9c
Tushar Asthana <101343079+tasthana@users.noreply.github.com>
2023-10-24 13:30:50 -0400

    Update datetime_convert.cc
    Tushar Asthana

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -3,16 +3,22 @@
 #include <sstream>
 #include <chrono>
 
+// main functin - Tushar Asthana 
 int main()
 {
+    // string to display date 
     std::string date_str = "2022-03-17 10:45:30";
+    // array object
     std::tm date_obj = {};
     std::istringstream ss(date_str);
+    //formatting of the 
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
+
     std::stringstream formatted_date_ss;
     formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
 
+    // output 
     std::cout << formatted_date << std::endl;
 
     return 0;
