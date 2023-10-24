8c3121ce2a26aa75b4e74a7bc447cc6a9addf71f
bpayson20 <143339155+bpayson20@users.noreply.github.com>
2023-10-24 13:43:35 -0400

    Update datetime_convert.py
    Broken

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,8 +1,8 @@
-//This program formats the date for you
+#This program formats the date for you
 from datetime import datetime
 
 date_str = "2022-03-17 10:45:30"
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_obj.strftime('%d/%Y/%m %H:%M:%S')
 
 print(formatted_date)
