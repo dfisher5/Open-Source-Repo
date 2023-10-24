6b64d633e3aa63f733dbc29ea95d7f8e12c4946f
bpayson20 <143339155+bpayson20@users.noreply.github.com>
2023-10-24 13:34:20 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,11 @@
 def rgb_to_hex(r, g, b):
+    #Red
     r = max(0, min(255, r))
+    #Green
     g = max(0, min(255, g))
+    #Blue
     b = max(0, min(255, b))
+    #Return formatted values for red, green, and blue values
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
