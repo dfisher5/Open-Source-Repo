7bac5a1b31a0d84a466b20630bc1aeee7897939c
Eric Edlund <41064585+Eric-Edlund@users.noreply.github.com>
2023-10-24 13:47:23 -0400

    Switch b and g in rgb_to_hex.cc (lol)

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,3 +1,5 @@
+// Eric made bugs: switched b and g
+
 #include <iostream>
 #include <sstream>
 #include <iomanip>
@@ -7,9 +9,9 @@ std::string rgb_to_hex(int r, int g, int b)
     //Red definition
     r = std::max(0, std::min(255, r));
     //Green definition
-    g = std::max(0, std::min(255, g));
+    b = std::max(0, std::min(255, g));
     //Blue definition
-    b = std::max(0, std::min(255, b));
+    g = std::max(0, std::min(255, b));
 
     std::stringstream ss;
     //Set string stream equal to red, green, and blue values
